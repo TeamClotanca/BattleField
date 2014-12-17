e7836884e1c168e9e25267f0fbd21f0a96786e8d
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-12-17 23:59:08 +0200

    Create MineType.cs

---
+++ MineType.cs
@@ -0,0 +1,50 @@
+namespace BattleField
+{
+    public static class MineType
+    {
+        public static readonly int[,] firstMine =
+        {
+            {0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0}
+        };
+
+        public static readonly int[,] secondMine =
+        {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0}
+        };
+
+        public static readonly int[,] thirdMine =
+        {
+            {0, 0, 1, 0, 0},
+            {0, 1, 1, 1, 0},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 1, 0, 0}
+        };
+
+        public static readonly int[,] fourthMine =
+        {
+            {0, 1, 1, 1, 0},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0}
+        };
+
+        public static readonly int[,] fivethMine =
+        {
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1}
+        };
+    }
+}
