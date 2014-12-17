dbad9454e3545ca31c0212a2eba73ed23b80f8e6
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-12-17 23:59:28 +0200

    Create StartGame.cs

---
+++ StartGame.cs
@@ -0,0 +1,15 @@
+namespace BattleField
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StartGame
+    {
+        public static void Main()
+        {
+            GameEngine.Run();
+        }
+    }
+}
